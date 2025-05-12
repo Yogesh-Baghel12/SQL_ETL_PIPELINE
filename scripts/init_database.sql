c6829c04b3f0a4fd75e650ca063c3a5db7c6bfc4
yogesh baghel <139314033+Yogesh-Baghel12@users.noreply.github.com>
2025-05-12 16:24:59 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,16 +1,22 @@
 use master;
+
+GO
 create database if not exists  DataWarehouse;
-
+GO
 use DataWarehouse;
-
+GO
 create schema if not exists bronze;
+GO
 create schema if not exists silver;
+GO
 create schema if not exists gold;
-
+GO
 
 
 if OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
    DROP TABLE bronze.crm_cust_info;
+GO
+	
 create table  bronze.crm_cust_info(
 cst_id INT,
 cst_key NVARCHAR(50),
@@ -20,12 +26,10 @@ cst_marital_status NVARCHAR(50),
 cst_gndr NVARCHAR(50),
 cst_create_date DATE
 );
-
-use DataWarehouse;
-
-
+GO
 if OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
    DROP TABLE bronze.crm_prd_info;
+GO
 create table bronze.crm_prd_info(
 prd_id INT,
 prd_key NVARCHAR(50),
@@ -35,9 +39,11 @@ prd_line NVARCHAR(50),
 prd_start_dt DATETIME,
 prd_end_dt DATETIME
 );
+GO
 
 if OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
    DROP TABLE bronze.crm_sales_details;
+GO
 create table bronze.crm_sales_details(
 sls_ord_num NVARCHAR(50),
 sls_prd_key NVARCHAR(50),
@@ -49,36 +55,43 @@ sls_sales INT,
 sls_quantity INT,
 sls_price INT
 );
-
+GO
 
 if OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
    DROP TABLE bronze.erp_loc_a101;
+GO
 create table bronze.erp_loc_a101(
 cid NVARCHAR(50),
 cntry NVARCHAR(50)
 );
+GO
 
 
 if OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
    DROP TABLE bronze.erp_cust_az12;
+GO
 create table bronze.erp_cust_az12(
 cid NVARCHAR(50),
 bdate DATE,
 gen NVARCHAR(50)
 );
+GO
 
 
 if OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
    DROP TABLE bronze.erp_px_cat_g1v2;
+GO
 create table bronze.erp_px_cat_g1v2(
 id NVARCHAR(50),
 cat NVARCHAR(50),
 subcat NVARCHAR(50),
 maintenance NVARCHAR(50)
 );
+GO
 
 
 EXEC bronze.load_bronze;
+GO
 
 
 CREATE OR ALTER PROCEDURE bronze.load_bronze
@@ -208,6 +221,5 @@ BEGIN
         PRINT '=========================================================';
     END CATCH
 END;
-
-
+GO
 
